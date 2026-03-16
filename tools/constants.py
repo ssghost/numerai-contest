717bdf64c98fee0ca43f8fe541c3c05e495ec28f
ssghost <ssprof0@gmail.com>
2026-03-16 21:23:12 +0800

    add neutralize

--- tools/constants.py
+++ tools/constants.py
@@ -1,5 +1,5 @@
 DATA_PATH = "data/live.parquet"
-RESULT_PATH = "data/submission.csv" 
+RESULT_PATH = "data/submission_neutralized.csv" 
 TRAIN_DATA = [
     "v5.2/train.parquet",      
     "v5.2/validation.parquet",  
